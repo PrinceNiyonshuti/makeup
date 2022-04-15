56808be71eecdf82309d09da8426a9b1cf0e33ec
PrinceNiyonshuti <npprince47@gmail.com>
2022-04-15 22:12:09 +0200

    Ft(Setup structure): setted up the app structure and pages needed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {/* <!-- loader --> */}
+      <div id="ftco-loader" className="show fullscreen">
+        <svg className="circular" width="48px" height="48px">
+          <circle className="path-bg" cx="24" cy="24" r="22" fill="none" strokeWidth="4" stroke="#eeeeee" />
+          <circle className="path" cx="24" cy="24" r="22" fill="none" strokeWidth="4" strokeMiterlimit="10" stroke="#F96D00" />
+        </svg>
+      </div>
+
     </div>
   );
 }
