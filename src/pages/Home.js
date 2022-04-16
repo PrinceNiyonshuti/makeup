f62f825bdc96dd1e2b2813b14cff2fcdcda68b93
PrinceNiyonshuti <npprince47@gmail.com>
2022-04-16 13:12:06 +0200

    ft(Reusable components): added our works component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,66 +1,15 @@
 import React from 'react'
-import { Link } from "react-router-dom";
 import Advert from '../components/Advert';
 import Experts from '../components/Experts';
 import Service from '../components/Service';
+import Works from '../components/works';
 function Home() {
   return (
     <div>
 		<Service />
 		<Experts/>
 		<Advert/>
-
-    <section className="ftco-section">
-    	<div className="container">
-    		<div className="row justify-content-center mb-5 pb-3">
-          <div className="col-md-7 heading-section text-center ">
-            <h2 className="mb-4">Our Work</h2>
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-          </div>
-        </div>
-        <div className="row">
-        	<div className="col-md-4 ">
-        		<Link to="#" className="work-entry">
-        			<img src="images/work-1.jpg" className="img-fluid" alt="Colorlib Template "/>
-        			<div className="info d-flex align-items-center">
-        				<div>
-        					<div className="icon mb-4 d-flex align-items-center justify-content-center">
-        						<span className="icon-search"></span>
-        					</div>
-		        			<h3>Weeding Makeup</h3>
-	        			</div>
-        			</div>
-        		</Link>
-        	</div>
-        	<div className="col-md-4 ">
-        		<Link to="#" className="work-entry">
-        			<img src="images/work-2.jpg" className="img-fluid" alt="Colorlib Template"/>
-        			<div className="info d-flex align-items-center">
-        				<div>
-        					<div className="icon mb-4 d-flex align-items-center justify-content-center">
-        						<span className="icon-search"></span>
-        					</div>
-		        			<h3>Bridal Shower Makeup</h3>
-	        			</div>
-        			</div>
-        		</Link>
-        	</div>
-        	<div className="col-md-4 ">
-        		<Link to="#" className="work-entry">
-                <img src="images/photoShoot.jpg" className="img-fluid" alt="Colorlib Template"/>
-        			<div className="info d-flex align-items-center">
-        				<div>
-        					<div className="icon mb-4 d-flex align-items-center justify-content-center">
-        						<span className="icon-search"></span>
-        					</div>
-		        			<h3>PhotoShoot Makeup</h3>
-	        			</div>
-        			</div>
-        		</Link>
-        	</div>
-        </div>
-    	</div>
-    </section>
+    	<Works/>
 
 	  <section className="ftco-section ftco-counter img" id="section-counter" style={{backgroundImage: "url(images/bg_4.jpg)"}}>
 			<div className="overlay"></div>
