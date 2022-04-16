d214630e90166b5f1ff5a0730a97cff6cfe2eb20
PrinceNiyonshuti <npprince47@gmail.com>
2022-04-16 13:19:46 +0200

    ft(About page): refactored about page to meet standards

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import Experts from "../components/Experts";
 function About() {
   return (
 		<div>
@@ -35,102 +35,7 @@ function About() {
 					</div>
 				</div>
 			</section>
-
-			<section className="ftco-section bg-light">
-				<div className="container">
-					<div className="row justify-content-center mb-5 pb-3">
-						<div className="col-md-7 heading-section  text-center">
-							<h2 className="mb-4">Our Beauty Experts</h2>
-							<p>
-								Far far away, behind the word mountains, far from the countries
-								Vokalia and Consonantia, there live the blind texts.
-							</p>
-						</div>
-					</div>
-					<div className="row">
-						<div className="col-lg-3 d-flex mb-sm-4 ">
-							<div className="staff">
-								<div
-                  className="img mb-4"
-                  style={{ backgroundImage: " url(images/person_1.jpg)" }}></div>
-							<div className="info text-center">
-									<h3>
-										<Link to="teacher-single.html">Mellisa Smith</Link>
-									</h3>
-									<span className="position">Stylist</span>
-									<div className="text">
-										<p>
-											Far far away, behind the word mountains, far from the
-											countries Vokalia and Consonantia, there live the blind
-											texts.
-										</p>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="col-lg-3 d-flex mb-sm-4 ">
-							<div className="staff">
-								<div
-                  className="img mb-4"
-                  style={{ backgroundImage: " url(images/person_2.jpg)" }}></div>
-							<div className="info text-center">
-									<h3>
-										<Link to="teacher-single.html">Marie Mush</Link>
-									</h3>
-									<span className="position">Fashionist</span>
-									<div className="text">
-										<p>
-											Far far away, behind the word mountains, far from the
-											countries Vokalia and Consonantia, there live the blind
-											texts.
-										</p>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="col-lg-3 d-flex mb-sm-4 ">
-							<div className="staff">
-								<div
-                  className="img mb-4"
-                  style={{ backgroundImage: " url(images/person_3.jpg)" }}></div>
-						<div className="info text-center">
-									<h3>
-										<Link to="teacher-single.html">Ana Jacobson</Link>
-									</h3>
-									<span className="position">Makeup Stylist</span>
-									<div className="text">
-										<p>
-											Far far away, behind the word mountains, far from the
-											countries Vokalia and Consonantia, there live the blind
-											texts.
-										</p>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="col-lg-3 d-flex mb-sm-4 ">
-							<div className="staff">
-								<div
-                  className="img mb-4"
-                  style={{ backgroundImage: " url(/images/person_4.jpg)" }}></div>
-    							<div className="info text-center">
-									<h3>
-										<Link to="teacher-single.html">Ivan Dorchsner</Link>
-									</h3>
-									<span className="position">Nail Specialist</span>
-									<div className="text">
-										<p>
-											Far far away, behind the word mountains, far from the
-											countries Vokalia and Consonantia, there live the blind
-											texts.
-										</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
+      <Experts/>
 		</div>
 	);
 }
